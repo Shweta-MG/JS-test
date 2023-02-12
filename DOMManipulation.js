a03f87027cce9718a5a7d78c7dff04215f1a2200
Shweta-MG <shweta.malav@gmail.com>
2023-02-12 12:55:19 +0100

    files added

---
+++ DOMManipulation.js
@@ -0,0 +1,8 @@
+//Using JavaScript, create a button, add it to the html
+let div = document.querySelector('.selfAssesment');
+let button = document.createElement('BUTTON');
+let text = document.createTextNode("Button");
+button.appendChild(text);
+div.appendChild(button);
+
+
